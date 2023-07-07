982a8fea8012ff2ae95de3f7bf8fcd4f6e7e3bae
nesaguilarp <nesaguilarp@gmail.com>
2023-07-07 10:52:31 -0700

    Pausa en Hermosillo

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const createtask = (evento) =>{
     evento.preventDefault()
     const input = document.querySelector(`[data-form-input]`)
     console.log(input.value);
+    const task = document.querySelector(`[data-task]`)
+    input.value = ``;
+    console.log(task);
 }
 console.log(btn);
 
