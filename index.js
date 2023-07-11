8e934130eddcf86540f7c9903d67b5715d63e62c
nesaguilarp <nesaguilarp@gmail.com>
2023-07-10 23:24:40 -0600

    opciones funcionando

--- index.js
+++ index.js
@@ -2,10 +2,19 @@ const btn = document.querySelector(`[data-form-btn]`);
 const createtask = (evento) =>{
     evento.preventDefault()
     const input = document.querySelector(`[data-form-input]`)
-    console.log(input.value);
-    const task = document.querySelector(`[data-task]`)
+    const value = input.value;
+    const list = document.querySelector("[data-list]")
+    const task = document.createElement(`li`);
+    task.classList.add("card")
     input.value = ``;
-    console.log(task);
+    const content = `<div>
+                    <i class="far fa-check-square icon"></i>
+                    <span class="task">${value}</span>
+                    </div>
+                    <i class="fas fa-trash-alt trashIcon icon"></i>`
+    task.innerHTML = content;
+    list.appendChild(task);
+    console.log(content);
 }
 console.log(btn);
 
